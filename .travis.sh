e88e5c1282983b781d928513cff5bab9b33f57b8
jjYBdx4IL <jjYBdx4IL@github.com>
2019-02-15 06:46:12 +0100

    test

--- .travis.sh
+++ .travis.sh
@@ -3,10 +3,11 @@
 set -Eev
 
 choco install unzip || :
+choco install zip || :
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sdk install java 11.0.2-open
 sdk install maven 3.6.0
-mvn clean install
+mvn -q clean install
 
